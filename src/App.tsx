b32950ed56e5e6d932035650c3549a5d28d4fe5f
przemekszcz7 <przemekszcz7@gmail.com>
2026-06-19 12:58:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,7 @@ export default function App() {
               </div>
             </div>
             <div className="h-[400px] rounded-3xl overflow-hidden border border-stone-tan bg-paper">
-              <iframe src="http://googleusercontent.com/maps.google.com/5" width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" title="Mapa" />
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.34720876402!2d21.268668612682635!3d52.400383244961525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471edb5ab30aaabf%3A0xc6f41acb80b1c224!2sKARCZMA%20RZ%C4%84DZA!5e0!3m2!1spl!2spl!4v1781866594454!5m2!1spl!2spl" width="100%" height="100%" style={{ border: 0 }} allowFullScreen loading="lazy" title="Mapa" />
             </div>
           </div>
         </section>
